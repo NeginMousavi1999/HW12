b546de0c84f565b3cd14d4fb8e7c53e55ecc6da8
negin <negin.s.mousavi@gmail.com>
2021-11-16 21:40:24 +0330

    add more loges for customer

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -2,6 +2,7 @@ package model;
 
 import enums.CookState;
 import enums.CustomerState;
+import enums.RestaurantState;
 import lombok.Data;
 
 import java.util.List;
@@ -24,58 +25,6 @@ public class Customer extends Person {
         return totalCustomersInRestaurant;
     }
 
-    public synchronized void enterToRestaurant(int restaurantCapacity) throws InterruptedException {
-        synchronized (totalCustomersInRestaurant) {
-            if (totalCustomersInRestaurant >= restaurantCapacity) {
-                System.out.println("Restaurant is full... please wait until someone leave");
-                this.wait();
-            }
-            System.out.println(this.pName + " entered");
-            totalCustomersInRestaurant++;
-            customerState = CustomerState.CUSTOMER_ENTERED;
-        }
-    }
-
-    @Override
-    public void run() {
-        this.customerState = CustomerState.CUSTOMER_STARTING;
-        synchronized (restaurant) {
-            while (restaurant.getCapacity() <= totalCustomersInRestaurant) {
-                try {
-                    System.out.println(pName + " wanted to entered but Restaurant is full... so please wait until someone leave");
-                    restaurant.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            totalCustomersInRestaurant++;
-            System.out.println(pName + " entered to restaurant");
-/*            //order done!
-            setCustomerState(CustomerState.PLACED_ORDER);
-            System.out.println(restaurant.getAvailableCook());
-
-            while (restaurant.getAvailableCook() == null) {//TODO test
-                System.out.println("i am waiting");
-                try {
-                    restaurant.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }*/
-            System.out.println(getPName() + " wait some times");
-            try {
-                restaurant.wait(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println(getPName() + " wake and " + "total: " + totalCustomersInRestaurant);
-            totalCustomersInRestaurant--;
-            restaurant.notify();
-
-
-        }
-    }
-
     @Override
     public String toString() {
         return "Customer{" +
@@ -85,19 +34,52 @@ public class Customer extends Person {
                 '}';
     }
 
-    public Cook getCookToAcceptOrder(List<Cook> availableCooks) {
-        synchronized (availableCooks) {
-            if (availableCooks.isEmpty()) {
-                System.out.println("we have n't available cook to accept your order... so please wait");
+    @Override
+    public void run() {
+        this.customerState = CustomerState.CUSTOMER_STARTING;
+        synchronized (restaurant) {
+            while (restaurant.getCapacity() <= totalCustomersInRestaurant) {
                 try {
-                    availableCooks.wait();
+                    System.out.println(pName + " wanted to entered but Restaurant is full... so please wait until someone leave");
+                    restaurant.setRestaurantState(RestaurantState.FULL);
+                    restaurant.wait();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-
-                return null;
             }
-            return availableCooks.remove(0);
+            totalCustomersInRestaurant++;
+            System.out.println(pName + " entered to restaurant");
+
+            //order done!
+            setCustomerState(CustomerState.PLACED_ORDER);
+            System.out.println(pName + " placed order");
+
+            Cook cook = restaurant.getAvailableCook();
+            while (cook == null) {//TODO test
+                System.out.println(pName + " is waiting because we have no available cook");
+                try {
+                    restaurant.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                cook = restaurant.getAvailableCook();
+            }
+            cook.setCookState(CookState.COOK_RECEIVED_ORDER);
+            System.out.println(cook.getPName() + " accepted " + getPName() + "'s order");//TODO
+
+            System.out.println(getPName() + " is eating ....");
+            try {
+                restaurant.wait(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            cook.setCookState(CookState.COOK_STARTING);//TODO must be in Cook
+            System.out.println(getPName() + " finished and gonna go out.. so the total will be:" + (totalCustomersInRestaurant - 1));
+            totalCustomersInRestaurant--;
+            if (totalCustomersInRestaurant == 0) {
+                restaurant.setRestaurantState(RestaurantState.CLOSE);
+            }
+            restaurant.notify();
         }
     }
 }
